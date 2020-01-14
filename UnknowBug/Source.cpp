ccfa0b745033565d9c9f59ee07463c06f2d20684
小白狗 <xiaobaigou2000@outlook.com>
2020-01-14 20:19:41 +0800

    ostringstream厉害了

--- UnknowBug/Source.cpp
+++ UnknowBug/Source.cpp
@@ -1,11 +1,13 @@
 ﻿#include<iostream>
 #include<vector>
 #include<chrono>
+#include<sstream>
 
 int main() {
     using std::cin;
     using std::cout;
     using std::vector;
+    using std::ostringstream;
     std::chrono::high_resolution_clock hrc;
     int low, high = 0;
     cin.tie(nullptr);
@@ -14,6 +16,7 @@ int main() {
 
     auto start = hrc.now();
     vector<int> primers;
+    vector<vector<int>> result;
     primers.reserve(1229);
     for (int j = 2; j <= high; j++) {
         int current = j;
@@ -29,25 +32,40 @@ int main() {
         if (current == j)
             primers.push_back(j);
     }
+
+    int maxRowLength = 0;
     for (int i = low; i <= high; ++i) {
-        cout << i << "=";
         int num = i;
+        result.push_back(vector<int>{});
+        auto back = result.rbegin();
+        back->push_back(num);
         for (const auto& j : primers) {
             while (num % j == 0) {
-                cout << j;
+                back->push_back(j);
                 num /= j;
-                if (num != 1) {
-                    cout << "*";
-                }
-                else
+                if (num == 1)
                 {
-                    cout << "\n";
                     break;
                 }
             }
-
         }
     }
+
+    ostringstream oss;
+    for (const auto& i : result)
+    {
+        auto begin = i.begin();
+        auto end = i.end();
+        oss << *begin++ << '=';
+        oss << *begin++;
+        for (auto j = begin; j < end; ++j)
+        {
+            oss << '*' << *j;
+        }
+        oss << '\n';
+    }
+    cout << oss.str();
+
     auto duration = hrc.now() - start;
     auto pastTime = std::chrono::duration_cast<std::chrono::milliseconds>(duration);
     cout << "\nusing " << pastTime.count() << " ms\n";

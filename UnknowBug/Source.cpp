7bf02f6aa7c067725801a7cae6909c8e725c6908
小白狗 <xiaobaigou2000@outlook.com>
2020-02-22 23:13:33 +0800

    stupid lanqiao question

--- UnknowBug/Source.cpp
+++ UnknowBug/Source.cpp
@@ -1,29 +1,50 @@
-﻿#include<iostream>
-#include<memory>
-using namespace std;
-
-struct UPTest
-{
-    int a;
-    int b;
-
-    UPTest():a(),b() { cout << "UPTest object constructed. [default constructor]\n"; }
-    UPTest(const UPTest& from) { a = from.a; b = from.b; cout << "UPTest copy constructor called.\n"; }
-    UPTest(UPTest&& from)noexcept { a = from.a; b = from.b; cout << "UPTest move constructor called.\n"; }
-    UPTest& operator=(const UPTest& from) { a = from.a; b = from.b; cout << "copy assignment called.\n"; return *this; }
-    UPTest& operator=(UPTest&& from)noexcept { a = move(from.a); b = move(from.b); cout << "move assignment called.\n"; return *this; }
-    ~UPTest() { cout << "UPTest object deleted.\n"; }
-};
+﻿#include<algorithm>
+#include<iostream>
+#include<vector>
+#include<random>
+#include<chrono>
 
 int main()
 {
-    UPTest hello;
-    UPTest world(hello);
-    UPTest third(std::move(hello));
-    UPTest forth = world;
-    UPTest fifth;
-    fifth = world;
-    world = move(fifth);
+    using namespace std;
+    default_random_engine dre(chrono::system_clock::now().time_since_epoch().count());
+    uniform_int_distribution<int> hello(0, 1000);
+    int toSort[100];
 
+    for (size_t i = 0; i < 100; i++)
+    {
+        toSort[i] = hello(dre);
+    }
+
+    int l = 3, r = 10, k = 2;
+    cout << "please input l(left boundry), r(right boundry), k\n";
+    cin >> l >> r >> k;
+
+    --l;
+    --k;
+    sort(begin(toSort) + l, begin(toSort) + r, [](int a, int b) {return a > b; });
+    auto beg = begin(toSort);
+    int result = *(begin(toSort) + l + k);
+    cout << "result is :" << result << '\n';
+    for (auto i = begin(toSort); i != begin(toSort) + l; ++i)
+    {
+        cout << *i << ' ';
+    }
+    cout << " [ ";
+    for (auto i = begin(toSort) + l; i != begin(toSort) + l + k; ++i)
+    {
+        cout << *i << ' ';
+    }
+    cout << '(' << *(begin(toSort) + l + k) << ") ";
+    for (auto i = begin(toSort) + l + k + 1; i != begin(toSort) + r; ++i)
+    {
+        cout << *i << ' ';
+    }
+    cout << " ] ";
+    for (auto i = begin(toSort) + r; i != begin(toSort) + r + 5; ++i)
+    {
+        cout << *i << ' ';
+    }
+    cout << endl;
     return 0;
 }

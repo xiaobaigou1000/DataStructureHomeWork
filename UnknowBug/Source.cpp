cd8584c85db3cac6b9eeaa10ab57cfc16cc7bc18
小白狗 <xiaobaigou2000@outlook.com>
2020-03-07 21:18:48 +0800

    another dead lock

--- UnknowBug/Source.cpp
+++ UnknowBug/Source.cpp
@@ -2,43 +2,55 @@
 #include<chrono>
 #include<thread>
 #include<mutex>
+#include<vector>
+#include<random>
+
+struct MutexedData
+{
+    std::mutex mut;
+    uint32_t data;
+};
 
 int main()
 {
     using namespace std;
     using namespace std::chrono;
     using namespace std::chrono_literals;
-    mutex loc1;
-    mutex loc2;
-    std::thread testA([&] {
-        while (true)
+
+    default_random_engine dre(system_clock::now().time_since_epoch().count());
+    uniform_int_distribution<uint32_t> uid(0, 100);
+    std::vector<MutexedData> datalist(20);
+    for (uint32_t i = 0; i < 20; ++i)
+    {
+        datalist[i].data = i;
+    }
+
+    thread a([&datalist] {
+        auto ptr = datalist.begin();
+        auto end = --datalist.end();
+        while (ptr != end)
         {
-            {
-                this_thread::sleep_for(500ms);
-                lock_guard<mutex> lg1(loc1);
-                cout << "lock 1\n";
-                this_thread::sleep_for(500ms);
-                lock_guard<mutex> lg2(loc2);
-                cout << "lock 2\n";
-            }
-            cout << "unlock\n";
+            lock_guard<mutex> la(ptr->mut);
+            lock_guard<mutex> lb((ptr + 1)->mut);
+            printf("thread a is now visiting %d\n", ptr->data);
+            ++ptr;
         }
         });
-    std::thread testB([&] {
-        while (true)
+
+    thread b([&datalist] {
+        auto ptr = datalist.rbegin();
+        auto end = --datalist.rend();
+        while (ptr != end)
         {
-            {
-                this_thread::sleep_for(3s);
-                lock_guard<mutex> lg1(loc2);
-                cout << "lock 2\n";
-                this_thread::sleep_for(3s);
-                lock_guard<mutex> lg2(loc1);
-                cout << "lock 1\n";
-            }
-            cout << "unlock\n";
+            lock_guard<mutex> la(ptr->mut);
+            lock_guard<mutex> lb((ptr + 1)->mut);
+            printf("thread b is now visiting %d\n", ptr->data);
+            ++ptr;
         }
         });
-    testA.join();
-    testB.join();
+
+    a.join();
+    b.join();
+
     return 0;
 }
